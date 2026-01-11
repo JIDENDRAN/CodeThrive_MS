f1556ef0e5f28e52fbc50efdbf0a83cd2de845ac
JIDENDRAN <jidendran2312003@gmail.com>
2026-01-11 13:49:37 +0530

    Frontend ready for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: "/", 
+  
   server: {
     host: true,         // allow access from network
     port: 5173,         // your dev server port
@@ -11,7 +13,8 @@ export default defineConfig({
       'localhost',
       '127.0.0.1',
       '.ngrok-free.app', // allow all Ngrok free subdomains
-      '.ngrok-free.dev'
+      '.ngrok-free.dev',
+       
     ]
   }
 });
