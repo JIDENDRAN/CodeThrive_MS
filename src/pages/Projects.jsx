877a7461fbdb0d6a3f630b8e88a3520424d35515
JIDENDRAN <jidendran2312003@gmail.com>
2026-01-24 15:34:57 +0530

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { AiOutlineEye, AiOutlineEdit, AiOutlineDelete } from "react-icons/ai";
 import { getProjects} from "../services/api";
 import { toast } from "react-toastify";
+import api from "../api/axios"; 
 
 
 const Projects = () => {
